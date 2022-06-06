1660cdbcb7e559fe170c0603076932643a0c78c2
malexander15 <creepinjezus15@gmail.com>
2022-06-06 14:34:16 -0400

    added active model to gemfile and required it in the Scheduler class file

--- Gemfile
+++ Gemfile
@@ -6,3 +6,5 @@ ruby "3.0.2"
 gem "sinatra"
 gem 'rake'
 gem 'rspec'
+gem 'activesupport'
+gem 'activemodel'
--- scheduler.rb
+++ scheduler.rb
@@ -1,7 +1,13 @@
 class Scheduler
+  require 'active_model'
+  extend ActiveModel::Naming
+  extend ActiveModel::Callbacks
+  include ActiveModel::Conversion
+  include ActiveModel::Validations
   attr_accessor :start_time, :end_time
 
   def initialize(start_time, end_time)
+
   end
 
   def schedule_meeting
